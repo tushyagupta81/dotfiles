48290b7f0bc6d99146d9eb0c92fd4682bb38eb54
Tushya <tushya007@gmail.com>
2024-05-11 17:18:40 +0530

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -17,8 +17,11 @@ fi
 
 # Verify brew is now accessible
 if ! command -v brew &>/dev/null; then
-    echo "Failed to configure Homebrew in PATH. Please add Homebrew to your PATH manually."
-    exit 1
+    # echo "Failed to configure Homebrew in PATH. Please add Homebrew to your PATH manually."
+    # exit 1
+    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 fi
 
 # Update Homebrew and Upgrade any already-installed formulae
