5b24e4ba8f678d476230013a459dc363cce3cb1a
Tushya Gupta <tushya@Tushyas-Laptop.local>
2024-05-31 10:28:13 +0530

    changed harpoon keys

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -13,6 +13,6 @@ return {
         vim.keymap.set("n", "<C-r>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-i>", function() harpoon:list():select(4) end)
     end
 }
