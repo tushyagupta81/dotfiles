4827fceb843aa4d941feacc18fe236e585c49726
Tushya Gupta <tushya@Tushyas-Laptop.local>
2024-06-01 11:04:22 +0530

    minor changes

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -110,5 +110,9 @@ return {
 				prefix = "",
 			},
 		})
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 	end,
 }
