0e8092706b3de60d99e3a782faeae5a4fb81f21b
Tushya Gupta <tushya@Tushyas-Laptop.local>
2024-07-20 16:15:23 +0530

    changed from oil to mini.files, changes ide-script, tmux changes(new kaymap)

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -81,20 +81,54 @@ return { -- Collection of various small independent plugins/modules
 		end,
 	},
 	{
-		"echasnovski/mini.map",
+		"echasnovski/mini.files",
 		version = false,
 		config = function()
-			local MiniMap = require("mini.map")
-			MiniMap.open()
-			local gitsigns_integration = MiniMap.gen_integration.gitsigns({
-				add = "GitSignsAdd",
-				change = "GitSignsChange",
-				delete = "GitSignsDelete",
+			require("mini.files").setup({
+				mappings = {
+					close = "q",
+					go_in = "l",
+					go_in_plus = "L",
+					go_out = "h",
+					go_out_plus = "H",
+					reset = "<BS>",
+					reveal_cwd = "@",
+					show_help = "g?",
+					synchronize = "=",
+					trim_left = "<",
+					trim_right = ">",
+				},
+				windows = {
+					-- Maximum number of windows to show side by side
+					max_number = math.huge,
+					-- Whether to show preview of file/directory under cursor
+					preview = true,
+					-- Width of focused window
+					width_focus = 50,
+					-- Width of non-focused window
+					width_nofocus = 25,
+					-- Width of preview window
+					width_preview = 50,
+				},
 			})
-			MiniMap.setup({ integrations = { gitsigns_integration } })
-			vim.keymap.set("n", "<leader>mc", MiniMap.close)
-			vim.keymap.set("n", "<leader>mf", MiniMap.toggle_focus)
-			vim.keymap.set("n", "<leader>mo", MiniMap.open)
+			vim.keymap.set("n", "-", ":lua MiniFiles.open()<CR>")
 		end,
 	},
+	-- {
+	-- 	"echasnovski/mini.map",
+	-- 	version = false,
+	-- 	config = function()
+	-- 		local MiniMap = require("mini.map")
+	-- 		MiniMap.open()
+	-- 		local gitsigns_integration = MiniMap.gen_integration.gitsigns({
+	-- 			add = "GitSignsAdd",
+	-- 			change = "GitSignsChange",
+	-- 			delete = "GitSignsDelete",
+	-- 		})
+	-- 		MiniMap.setup({ integrations = { gitsigns_integration } })
+	-- 		vim.keymap.set("n", "<leader>mc", MiniMap.close)
+	-- 		vim.keymap.set("n", "<leader>mf", MiniMap.toggle_focus)
+	-- 		vim.keymap.set("n", "<leader>mo", MiniMap.open)
+	-- 	end,
+	-- },
 }
