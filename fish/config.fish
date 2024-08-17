8886753aec4f970ebc89165fc0eef2ce93c6a1b6
Tushya Gupta <tushyagupta81@gmail.com>
2024-08-17 20:34:29 +0530

    changes to fish config, making more modular

--- fish/config.fish
+++ fish/config.fish
@@ -30,23 +30,20 @@ abbr --add gp git push
 abbr --add gc git commit -m
 
 if test -d /home/linuxbrew/.linuxbrew
-  # Homebrew is installed on Linux
-
   set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
-  set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
-  set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
-  set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH
-  set -q MANPATH; or set MANPATH ''
-  set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
-  set -q INFOPATH; or set INFOPATH ''
-  set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
-
-  # Homebrew asked for this in order to `brew upgrade`
-  # set -gx HOMEBREW_GITHUB_API_TOKEN {api token goes here, don't remember where that's created}
 else if test -d /opt/homebrew
   # Homebrew is installed on MacOS
-  /opt/homebrew/bin/brew shellenv | source
+  set -gx HOMEBREW_PREFIX "/opt/homebrew"
 end
+
+set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+set -gx PATH "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin" $PATH
+set -q MANPATH; or set MANPATH ''
+set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH
+set -q INFOPATH; or set INFOPATH ''
+set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
+$HOMEBREW_PREFIX/bin/brew shellenv | source
 
 starship init fish | source
 thefuck --alias | source
