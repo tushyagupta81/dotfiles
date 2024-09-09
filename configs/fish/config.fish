c797bef8149ec3e206adf93c5d683b97dcddacc5
Tushya Gupta <tushyagupta81@gmail.com>
2024-09-09 19:43:58 +0530

    made changes to fish by accident

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -33,6 +33,31 @@ abbr --add gp git push
 abbr --add gc git commit -m
 abbr --add gr git add .\;git commit -m
 
+if test -d /home/linuxbrew/.linuxbrew
+  set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+else if test -d /opt/homebrew
+  set -gx HOMEBREW_PREFIX "/opt/homebrew"
+end
+
+set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
+set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/Homebrew"
+set -gx PATH "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin" $PATH
+set -q MANPATH; or set MANPATH ''
+set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH
+set -q INFOPATH; or set INFOPATH ''
+set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
+$HOMEBREW_PREFIX/bin/brew shellenv | source
+
+starship init fish | source
+thefuck --alias | source
+zoxide init fish --cmd cd | source
+fzf --fish | source
+
+if status --is-login
+  if not set -q TMUX
+    command fastfetch -c ~/dotfiles/fastfetch_conf.jsonc;
+  end
+end
 
 if status is-interactive
   # Commands to run in interactive sessions can go here
