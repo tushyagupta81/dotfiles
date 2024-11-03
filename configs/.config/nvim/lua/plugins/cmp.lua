5a0cd32010bd127241fdb74a746738e8b1e2b976
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-03 20:36:42 +0530

    some changes to cmp

--- configs/.config/nvim/lua/plugins/cmp.lua
+++ configs/.config/nvim/lua/plugins/cmp.lua
@@ -12,13 +12,64 @@ return {
 			},
 
 			sources = {
+				accept = { auto_brackets = { enabled = true } },
 				completion = {
 					enabled_providers = { "lsp", "path", "snippets", "buffer" },
 				},
-				accept = { auto_brackets = { enabled = true } },
+				providers = {
+					lsp = {
+						name = "LSP",
+						module = "blink.cmp.sources.lsp",
+
+						--- *All* of the providers have the following options available
+						--- NOTE: All of these options may be functions to get dynamic behavior
+						--- See the type definitions for more information
+						enabled = true, -- whether or not to enable the provider
+						transform_items = nil, -- function to transform the items before they're returned
+						should_show_items = true, -- whether or not to show the items
+						max_items = nil, -- maximum number of items to return
+						min_keyword_length = 0, -- minimum number of characters to trigger the provider
+						fallback_for = {}, -- if any of these providers return 0 items, it will fallback to this provider
+						score_offset = 0, -- boost/penalize the score of the items
+						override = nil, -- override the source's functions
+					},
+					path = {
+						name = "Path",
+						module = "blink.cmp.sources.path",
+						score_offset = 3,
+						opts = {
+							trailing_slash = false,
+							label_trailing_slash = true,
+							get_cwd = function(context)
+								return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+							end,
+							show_hidden_files_by_default = false,
+						},
+					},
+					snippets = {
+						name = "Snippets",
+						module = "blink.cmp.sources.snippets",
+						score_offset = -3,
+						opts = {
+							friendly_snippets = true,
+							search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+							global_snippets = { "all" },
+							extended_filetypes = {},
+							ignored_filetypes = {},
+						},
+
+						--- Example usage for disabling the snippet provider after pressing trigger characters (i.e. ".")
+						-- enabled = function(ctx) return ctx ~= nil and ctx.trigger.kind == vim.lsp.protocol.CompletionTriggerKind.TriggerCharacter end,
+					},
+					buffer = {
+						name = "Buffer",
+						module = "blink.cmp.sources.buffer",
+						fallback_for = { "lsp" },
+					},
+				},
 
 				-- experimental signature help support
-				trigger = { signature_help = { enabled = true } },
+				-- trigger = { signature_help = { enabled = true } },
 			},
 
 			windows = {
@@ -27,10 +78,32 @@ return {
 					max_width = 40,
 					max_height = 15,
 					border = "rounded",
-					draw = "reversed",
+					-- draw = "reversed",
 					scrolloff = 2,
 					direction_priority = { "s", "n" },
 					winblend = 0,
+					draw = function(ctx)
+						-- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/types.lua#L1-L6
+						-- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/windows/autocomplete.lua#L298-L349
+						-- differentiate LSP snippets from user snippets and emmet snippets
+						local source, client = ctx.item.source_id, ctx.item.client_id
+						if client and vim.lsp.get_client_by_id(client).name == "emmet_language_server" then
+							source = "emmet"
+						end
+
+						local sourceIcons = { snippets = "󰩫", buffer = "󰦨", emmet = "󰯸" }
+						local icon = sourceIcons[source] or ctx.kind_icon
+
+						return {
+							{
+								" " .. ctx.item.label .. " ",
+								fill = true,
+								hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+								max_width = 40,
+							},
+							{ icon .. " ", hl_group = "BlinkCmpKind" .. ctx.kind },
+						}
+					end,
 				},
 				documentation = {
 					min_width = 15,
