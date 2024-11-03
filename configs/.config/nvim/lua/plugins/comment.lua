e91f5ca157b2c97b0222b2641b38b95c76212d6c
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-03 17:56:16 +0530

    nvim plugins disabled according to use

--- configs/.config/nvim/lua/plugins/comment.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"numToStr/Comment.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
-	lazy = false,
-}
--- configs/.config/nvim/lua/plugins/mini.lua
+++ configs/.config/nvim/lua/plugins/mini.lua
@@ -25,7 +25,7 @@ return { -- Collection of various small independent plugins/modules
 			-- local statusline = require("mini.statusline")
 			-- -- set use_icons to true if you have a Nerd Font
 			-- statusline.setup({ use_icons = true })
-			--
+
 			-- -- You can configure sections in the statusline by overriding their
 			-- -- default behavior. For example, here we set the section for
 			-- -- cursor location to LINE:COLUMN
@@ -33,6 +33,7 @@ return { -- Collection of various small independent plugins/modules
 			-- statusline.section_location = function()
 			-- 	return "%2l:%-2v"
 			-- end
+
 			require("mini.indentscope").setup({
 				draw = {
 					delay = 100,
