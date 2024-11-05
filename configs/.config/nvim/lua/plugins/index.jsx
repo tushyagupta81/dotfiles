b1398e758ffcde65cc2cc5f3928be5f4b53072ad
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-05 10:16:37 +0530

    nvim-cmp works just fine my guy

--- configs/.config/nvim/lua/plugins/index.jsx
+++
@@ -1,9 +0,0 @@
-import { css } from "@emotion/core";
-
-export const index = () => {
-  return (
-    <div css={css``}>
-      <h1>print('hello world')</h1>
-    </div>
-  );
-};
--- configs/.config/nvim/lua/plugins/lsp.lua
+++ configs/.config/nvim/lua/plugins/lsp.lua
@@ -2,25 +2,27 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
-		-- "hrsh7th/cmp-nvim-lsp",
-		-- "hrsh7th/cmp-buffer",
-		-- "hrsh7th/cmp-path",
-		-- "hrsh7th/cmp-cmdline",
-		-- "hrsh7th/nvim-cmp",
-		-- "rafamadriz/friendly-snippets",
-		-- "L3MON4D3/LuaSnip",
-		-- "saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"rafamadriz/friendly-snippets",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
 	},
 	event = "LspAttach",
 	config = function()
 		local lspconfig = require("lspconfig")
 
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
 		local capabilities = vim.tbl_deep_extend(
 			"force",
 			{},
-			vim.lsp.protocol.make_client_capabilities()
-			-- cmp_lsp.default_capabilities()
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
 		)
 		require("fidget").setup({})
 		require("mason-lspconfig").setup({
@@ -31,7 +33,7 @@ return {
 					})
 				end,
 				["lua_ls"] = function()
-					lspconfig.lua_ls = {
+					lspconfig.lua_ls.setup({
 						capabilities = capabilities,
 						settings = {
 							Lua = {
@@ -40,7 +42,7 @@ return {
 								},
 							},
 						},
-					}
+					})
 				end,
 				["emmet_language_server"] = function()
 					lspconfig.emmet_language_server = {
@@ -73,38 +75,38 @@ return {
 				end,
 			},
 		})
-		-- require("luasnip.loaders.from_vscode").lazy_load()
-		-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		require("luasnip.loaders.from_vscode").lazy_load()
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-		-- cmp.setup({
-		-- 	-- Auto selects first item in completion list
-		-- 	completion = {
-		-- 		completeopt = "menu,menuone,noinsert",
-		-- 	},
-		-- 	snippet = {
-		-- 		expand = function(args)
-		-- 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-		-- 		end,
-		-- 	},
-		-- 	window = {
-		-- 		completion = cmp.config.window.bordered(),
-		-- 		documentation = cmp.config.window.bordered(),
-		-- 	},
-		-- 	mapping = cmp.mapping.preset.insert({
-		-- 		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-		-- 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-		-- 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
-		-- 		["<C-Space>"] = cmp.mapping.complete(),
-		-- 	}),
-		-- 	sources = cmp.config.sources({
-		-- 		{ name = "nvim_lsp" },
-		-- 		{ name = "luasnip" }, -- For luasnip users.
-		-- 		{ name = "codeium" },
-		-- 		{ name = "copilot" },
-		-- 	}, {
-		-- 		{ name = "buffer" },
-		-- 	}),
-		-- })
+		cmp.setup({
+			-- Auto selects first item in completion list
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+				{ name = "codeium" },
+				{ name = "copilot" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
 		vim.diagnostic.config({
 			-- update_in_insert = true,
