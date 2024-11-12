90a1b9e3b9587bc1353f82027f9b2e2044fce9de
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-12 17:46:45 +0530

    some freezing doing cpp

--- configs/.config/nvim/lua/plugins/dadbod.lua
+++ configs/.config/nvim/lua/plugins/dadbod.lua
@@ -1,15 +1,8 @@
 return {
 	"kristijanhusak/vim-dadbod-ui",
-	dependencies = {
-		{ "tpope/vim-dadbod", lazy = true },
-		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-	},
-	cmd = {
-		"DBUI",
-		"DBUIToggle",
-		"DBUIAddConnection",
-		"DBUIFindBuffer",
-	},
+	"tpope/vim-dadbod",
+	"kristijanhusak/vim-dadbod-completion",
+	lazy = true,
 	init = function()
 		-- Your DBUI configuration
 		vim.g.db_ui_use_nerd_fonts = 1
