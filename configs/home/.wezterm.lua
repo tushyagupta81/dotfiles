37fb46acffa2d88578e6bb6f51c3e0e32e788077
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-14 19:20:44 +0530

    kitty protocol try and nushell config not changing to it

--- configs/home/.wezterm.lua
+++ configs/home/.wezterm.lua
@@ -21,6 +21,8 @@ config.font_size = is_linux() and 12 or 16
 
 config.enable_tab_bar = false
 
+config.enable_kitty_graphics = true
+
 config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = 0,
