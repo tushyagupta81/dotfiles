54302d8a08ee6a35ed34bc9b00c3dc22f245c1e5
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-19 11:55:17 +0530

    harpoon in back bois

--- configs/.config/nvim/lua/plugins/harpoon.lua
+++ configs/.config/nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,5 @@
 return {
 	"ThePrimeagen/harpoon",
-	enabled = false,
 	event = "VeryLazy",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
@@ -12,9 +11,29 @@ return {
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
+
+		-- basic telescope configuration
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
 		vim.keymap.set("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
+			toggle_telescope(harpoon:list())
+		end, { desc = "Open harpoon window" })
 
 		vim.keymap.set("n", "<C-r>", function()
 			harpoon:list():select(1)
