bbb2866c9e6ccb044afd2275c91e74c46dde5706
Tushya Gupta <tushyagupta81@gmail.com>
2024-12-02 15:14:13 +0530

    trouble.nvim changes

--- configs/.config/nvim/lua/plugins/trouble.lua
+++ configs/.config/nvim/lua/plugins/trouble.lua
@@ -4,5 +4,7 @@ return {
 	config = function()
 		require("trouble").setup({})
 		vim.keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>")
+		vim.keymap.set("n", "]t", "<cmd>Trouble diagnostics next follow=true<cr>")
+		vim.keymap.set("n", "[t", "<cmd>Trouble diagnostics prev follow=true<cr>")
 	end,
 }
