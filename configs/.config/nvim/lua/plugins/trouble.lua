6087ffa690cf49501f0a1631645ef3293ffededb
Tushya Gupta <tushyagupta81@gmail.com>
2024-12-02 16:29:37 +0530

    trouble remaps and using only rustup now

--- configs/.config/nvim/lua/plugins/trouble.lua
+++ configs/.config/nvim/lua/plugins/trouble.lua
@@ -2,9 +2,20 @@ return {
 	"folke/trouble.nvim",
 	event = "VeryLazy",
 	config = function()
-		require("trouble").setup({})
+		require("trouble").setup({
+			auto_close = true, -- auto close when there are no items
+			auto_open = false, -- auto open when there are items
+			modes = {
+				mydiags = {
+					mode = "diagnostics", -- inherit from diagnostics mode
+					filter = {
+						severity = vim.diagnostic.severity.ERROR, -- errors only
+					},
+				},
+			},
+		})
 		vim.keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>")
-		vim.keymap.set("n", "]t", "<cmd>Trouble diagnostics next follow=true<cr>")
-		vim.keymap.set("n", "[t", "<cmd>Trouble diagnostics prev follow=true<cr>")
+		vim.keymap.set("n", "]t", "<cmd>Trouble mydiags next follow=true focus=true<cr>")
+		vim.keymap.set("n", "[t", "<cmd>Trouble mydiags prev follow=true focus=true<cr>")
 	end,
 }
