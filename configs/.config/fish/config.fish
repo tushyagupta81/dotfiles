72764eaeb9da758a7676fb83986b72d15292a1e5
Tushya Gupta <tushyagupta81@gmail.com>
2024-12-30 18:04:07 +0530

    added a alias for git pull in both dotfiles and programs folder

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -18,7 +18,7 @@ alias envl "l $HOME/.virtualenv"
 alias ide "tmux"
 alias wsl-open "bash wsl-open"
 alias gl "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
-
+alias gg "cd ~/dotfiles/;git pull;cd ~/programs/;git pull;cd"
 
 abbr --erase (abbr --list)
 
