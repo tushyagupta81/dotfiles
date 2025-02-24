d726caec212e43ba39f9b9a0307ff4e4b83195f5
Tushya Gupta <tushyagupta81@gmail.com>
2025-02-24 02:18:16 +0000

    changes from terminal mode in phone

--- exec/terminal.sh
+++ exec/terminal.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-packages=$(grep -o -E "(\w|')+" "$HOME/dotfiles/packages/packages_terminal.txt" | sed -e "s/'.*\$//" | sort -u -f)
-sudo pacman -S ${packages_arch[*]} --disable-sandbox
+packages=$(awk '{printf "%s ", $0}' $HOME/dotfiles/packages/packages_terminal.txt)
+sudo pacman -S ${packages[*]} --disable-sandbox
