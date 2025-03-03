5516cc84bffb5f1d6fe96f1b36de083d25c47feb
Tushya Gupta <tushyagupta81@gmail.com>
2025-03-03 14:52:06 +0530

    yazi here

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -68,6 +68,15 @@ if test -n "$HOMEBREW_PREFIX"
   $HOMEBREW_PREFIX/bin/brew shellenv | source
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 # tools init
 starship init fish | source
 zoxide init fish --cmd cd | source
